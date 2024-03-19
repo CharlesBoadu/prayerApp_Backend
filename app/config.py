e72b6ae1a9f58d28d47933b3ca3e0c22660ab3c1
Charles Boadu <60857017+CharlesBoadu@users.noreply.github.com>
2024-03-19 09:57:44 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,15 +3,6 @@
 # import urllib.parse as up
 # import psycopg2
 
-# up.uses_netloc.append("postgres")
-# url = up.urlparse(os.environ["postgres://janbgdhk:J8VKGaqgF2XuTMTNOLiF-q8GLOTq6usH@ruby.db.elephantsql.com/janbgdhk"])
-# conn = psycopg2.connect(database=url.path[1:],
-# user=url.janbgdhk,
-# password=url.J8VKGaqgF2XuTMTNOLiF-q8GLOTq6usH,
-# host=url.hostname,
-# port=url.port
-# )
-
 
 #Configurations for connecting to a database using SQLite
 # DATABASE_URI = 'sqlite:///app.db'
