ef1045801f841977d47744e39d82919aa8865c7d
Charles Boadu <charles.boadu@stu.ucc.edu.gh>
2024-03-24 00:31:20 +0000

    Add user API endpoints and corresponding service methods

--- app/controllers/v1/users.py
+++ app/controllers/v1/users.py
@@ -13,3 +13,30 @@ def getUsers():
     else:
         return jsonify(result), 401
     
+@app.route('/api/v1/user/<int:id>', methods=['GET'])
+def getUserById(id):
+    getUsersService = UsersService()
+    result = getUsersService.get_user(request,id)
+    if result.get("statusCode") == response_codes["SUCCESS"]:
+        return jsonify(result), 200
+    else:
+        return jsonify(result), 401
+    
+@app.route('/api/v1/user/<int:id>', methods=['DELETE'])
+def deleteUser(id):
+    deleteUserService = UsersService()
+    result = deleteUserService.delete_user(request,id)
+    if result.get("statusCode") == response_codes["SUCCESS"]:
+        return jsonify(result), 200
+    else:
+        return jsonify(result), 401
+    
+@app.route('/api/v1/user/<int:id>', methods=['PUT'])
+def updateUser(id):
+    updateUserService = UsersService()
+    result = updateUserService.update_user(request,id)
+    if result.get("statusCode") == response_codes["SUCCESS"]:
+        return jsonify(result), 200
+    else:
+        return jsonify(result), 401
+    
--- app/services/v1/users_service.py
+++ app/services/v1/users_service.py
@@ -52,3 +52,92 @@ class UsersService:
                     }
 
         return response
+    
+    def get_user(self,request,id): 
+        """
+            name: get_user
+            params: request
+            description: get user by id
+            dependencies:psycopg2
+            references:
+        """
+        connection = self.get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        if user:
+            response = {
+                "statusCode": response_codes["SUCCESS"],
+                "message": "User retrieved successfully",
+                'data': {
+                    "id": user[0],
+                    "first_name": user[1],
+                    "last_name": user[2],
+                    "email": user[3],
+                    "age": user[4],
+                    "phone": user[5],
+                    "role": user[6],
+                }
+            }
+            return response
+        else:
+            return {"statusCode": response_codes["USER_NOT_FOUND"], "message": "User does not exist"}
+        
+
+    def delete_user(self,request,id):
+        """
+            name: delete_user
+            params: request
+            description: delete user by id
+            dependencies:psycopg2
+            references:
+        """
+        connection = self.get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+        if user:
+            cursor.execute("DELETE FROM users WHERE id = %s", (id,))
+            connection.commit()
+            return {"statusCode": response_codes["SUCCESS"], "message": "User deleted successfully"}
+        else:
+            return {"statusCode": response_codes["USER_NOT_FOUND"], "message": "User does not exist"}
+        
+    def update_user(self,request,id):
+        """
+            name: update_user
+            params: request
+            description: update user by id
+            dependencies:psycopg2
+            references:
+        """
+        connection = self.get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+
+        data = request.json
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        email = data.get('email')
+        age = data.get('age')
+        phone = data.get('phone')
+        role = data.get('role')
+
+        if user:
+            cursor.execute('UPDATE users SET first_name = %s, last_name = %s, email = %s, age = %s, phone = %s, role = %s WHERE id = %s',
+                        (first_name,
+                        last_name,
+                        email,
+                        age,
+                        phone,
+                        role,
+                        id)
+                        )
+            connection.commit()
+            return {"statusCode": response_codes["SUCCESS"], "message": "User updated successfully"}
+        else:
+            return {"statusCode": response_codes["USER_NOT_FOUND"], "message": "User does not exist"}
